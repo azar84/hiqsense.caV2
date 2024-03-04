7a87c08ad8de8185cc48f3c9ccdbf49b844305a5
Azar Obeidat <121298185+azar84@users.noreply.github.com>
2024-03-03 18:37:44 -0600

    Revert "fixed the hero section on the mobile view"

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         "txt-primary": "#0A0628",
       },
       screens: {
-        'md': '930px',
+        'md': '900px',
         'sm':'200px'
       },
       gridTemplateColumns: {
